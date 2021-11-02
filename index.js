8283300b65eb068d87f4baa8cf350c9fcac260c7
DwikiCahyo <70952085+DwikiCahyo@users.noreply.github.com>
2021-11-03 00:54:34 +0700

    commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+
+//routing
+app.get('/', (req, res) => { //Halaman Root
+  res.send('Hello World!')
+})
+
+app.get('/halaman1', (req, res) => { //Halaman 1
+    // res.send('Halaman1')
+    res.json({
+        nama :'Darman',
+        Alamat:'Jauh',
+        email:' darman @gmail.com',
+
+    })
+  })
+
+ app.get('/halaman2', (req, res) => { //Halaman 2
+     res.send('Halaman2')
+  })
+
+  app.use('/', (req, res) => { //Menggunakan Use untuk request apapun
+      res.send('Halaman tidak ditemukan');
+      
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
