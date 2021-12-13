418bbde9d36c4613a1515af45d6ae85cc8728f83
DwikiCahyo <70952085+DwikiCahyo@users.noreply.github.com>
2021-12-13 14:54:03 +0700

    commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+const bodyParser = require('body-parser')
+const { json } = require('express')
 const express = require('express')
 const app = express()
 const port = 3000
 
-
+app.use(bodyParser.json())
 
 //routing
 app.get('/', (req, res,err) => { //Halaman Root
@@ -10,35 +12,36 @@ app.get('/', (req, res,err) => { //Halaman Root
 })
 
 app.get('/halaman1', (req, res) => { //Halaman 1
-    // res.send('Halaman1')
+    res.send('Halaman 1');
+    });
+
+app.post('/user', (req, res) => {
     res.json({
-        nama :'Darman',
-        Alamat:'Jauh',
-        email:' darman @gmail.com',
-
+      message:'Create NEW USER:POST /user',
+      body:req.body
     })
-  })
+});
 
-  app.get('/halaman3/:id', (req, res) => {
-    // res.send('Halaman 3 memiliki id ' + req.query.category);
-    res.send(`Halaman 3 memiliki id ${req.query.category}`);
+//   app.get('/halaman3/:id', (req, res) => {
+//     // res.send('Halaman 3 memiliki id ' + req.query.category);
+//     res.send(`Halaman 3 memiliki id ${req.query.category}`);
     
-  });
-  app.get('/halaman3', (req, res) => {
-    // res.send('Halaman 3 memiliki id ' + req.query.category);
-    res.send(`Halaman 3 `);
+//   });
+//   app.get('/halaman3', (req, res) => {
+//     // res.send('Halaman 3 memiliki id ' + req.query.category);
+//     res.send(`Halaman 3 `);
     
-  });
- app.get('/halaman2', (req, res) => { //Halaman 2
-     res.send('Halaman2')
-  })
+//   });
+//  app.get('/halaman2', (req, res) => { //Halaman 2
+//      res.send('Halaman2')
+//   })
 
-  app.use('/', (req, res) => { //Menggunakan Use untuk request apapun
-      res.send('Halaman tidak ditemukan');
+//   app.use('/', (req, res) => { //Menggunakan Use untuk request apapun
+//       res.send('Halaman tidak ditemukan');
       
-  });
+//   });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Localhost berhasil dijalankan pada http://localhost:${port}`)
 })
 
